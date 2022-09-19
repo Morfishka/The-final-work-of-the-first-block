c86e899de4c491a7890c8ca0a807506447f959f9
Morfishka <salimyanova.tany@mail.ru>
2022-09-19 17:49:39 +0500

    реальзация блок-схемы

--- Контрольная работа/Program.cs
+++ Контрольная работа/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите текст: ");
+string? text = Console.ReadLine();
+
+string StringsLess4Сharacters(string text)
+{
+    string [] words = text.Split(new char[]{',','!','?',' ',':'},StringSplitOptions.RemoveEmptyEntries);
+    Console.Write("Результат: ");
+    foreach(string word in words)
+    {
+        if (word.Length <= 3)
+        {
+            Console.Write($"{word} ");
+        }
+    }
+    return "";
+}
+
+StringsLess4Сharacters(text);
